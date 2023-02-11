e732ac5a7fd7c75bc02c68b791d6dcb72cc3e69b
Dumidu880 <124751175+Dumidu880@users.noreply.github.com>
2023-02-11 18:12:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoTyping = true  //auto tying in gc (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94719225807'] //ur owner number
-global.ownername = "©Qᴜᴇᴇɴ-x ᴡᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ" //ur owner name
-global.ytname = "ʏᴜʀᴇꜱʜ" //ur yt chanel name
-global.socialm = "👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻" //ur github or insta name
+global.owner = ['94742442932'] //ur owner number
+global.ownername = "©𝙳𝚄𝙼𝙸𝙳𝚄" //ur owner name
+global.ytname = "divert flowers" //ur yt chanel name
+global.socialm = "👸🏻Dumidu👸🏻" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.ownernomer = "94719225807" //ur number
-global.premium = ['94719225807'] //ur premium number
-global.botname = '©Qᴜᴇᴇɴ-x ᴡᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ' //ur bot name
-global.linkz = "https://chat.whatsapp.com/Fd5MPEBn7NQ1aviL4V5fa7" //your theme url which will be displayed on whatsapp
+global.ownernomer = "94742442932" //ur number
+global.premium = ['94772400336'] //ur premium number
+global.botname = '©𝚍𝚒𝚟𝚎𝚛𝚝 𝚏𝚕𝚘𝚠𝚎𝚛𝚜' //ur bot name
+global.linkz = "https://chat.whatsapp.com/EHQ0TLYehme6WBi9FYBNYn" //your theme url which will be displayed on whatsapp
 global.websitex = "https:// no" //ur website to be displayed
-global.botscript = 'https://github.com/yureshkav/Queen-X' //script link
-global.themeemoji = "🛸" //ur theme emoji
-global.packname = "   👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻" //ur sticker watermark packname
-global.author = "   👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻\n\n\nA whatsapp bot developed by\nCYBORG ROBOT" //ur sticker watermark author
-global.wm = "👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻." //ur watermark
+global.botscript = 'https://youtube.com/@divertflowers5821' //script link
+global.themeemoji = "🚀" //ur theme emoji
+global.packname = "   👸🏻𝙳𝚄𝙼𝙸𝙳𝚄👸🏻" //ur sticker watermark packname
+global.author = "   👸🏻𝙳𝚄𝙼𝙸𝙳𝚄 ＢＯＴ👸🏻\n\n\nA whatsapp bot developed by\nCYBORG ROBOT" //ur sticker watermark author
+global.wm = "👸🏻𝙳𝚄𝙼𝙸𝙳𝚄 ＢＯＴ👸🏻." //ur watermark
 
 // Other
 global.sessionName = 'session'
